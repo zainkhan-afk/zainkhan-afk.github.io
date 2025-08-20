bfa770d4e215c3b48c3171861ba2196d106b16c8
Zain <zain.9496@gmail.com>
2025-08-20 18:23:15 +0500

    Fixed markdown rendering. Also added html rendering support

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,6 +1,8 @@
 import { getAllPosts, getPostBySlug } from "@/lib/posts";
-import { remark } from "remark";
-import html from "remark-html";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import rehypeHighlight from "rehype-highlight";
 
 export async function generateStaticParams() {
   const posts = getAllPosts();
@@ -8,20 +10,26 @@ export async function generateStaticParams() {
 }
 
 export default async function PostPage({ params }) {
-  const { slug } = params;
+  const { slug } = await params;
   const post = getPostBySlug(slug);
-
-  const processedContent = await remark().use(html).process(post.content);
-  const contentHtml = processedContent.toString();
-
+  
+  // const processedContent = await remark().use(html).process(post.content);
+  // const contentHtml = processedContent.toString();
+  
   return (
-    <div className="max-w-3xl mx-auto py-12">
+    <div className="max-w-5xl w-full mx-auto py-12">
       <h1 className="text-4xl font-bold mb-4">{post.metadata.title}</h1>
       <p className="text-sm text-gray-400 mb-8">{post.metadata.date}</p>
-      <article
-        className="prose prose-invert"
-        dangerouslySetInnerHTML={{ __html: contentHtml }}
-      />
+
+      <article className="max-w-full prose prose-invert">
+        <ReactMarkdown
+         remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, rehypeHighlight]}
+        >
+          {post.content}
+        </ReactMarkdown>
+      </article>
+      
     </div>
   );
 }
