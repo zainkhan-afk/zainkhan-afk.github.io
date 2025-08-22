c0c6c7bf89c161dc2798b6f7eb2df869e5f88cd8
Zain <zain.9496@gmail.com>
2025-08-22 16:22:47 +0500

    Added processing sketches from online sources.

---
+++ src/components/GlobalP5Wrapper.js
@@ -0,0 +1,10 @@
+export default function GlobalP5Wrapper() {
+  return (
+    <iframe
+      src="https://zainkhan-afk.github.io/Simulations/Boids/index.html"
+    //   width="100%"
+      className="absolute inset-0 w-full h-full z-0"
+      style={{ border: "none" }}
+    ></iframe>
+  );
+}
