09ae785b863538091512affcf129885f89e28519
Zain <zain.9496@gmail.com>
2025-08-22 14:19:40 +0500

    Added more profile information

--- src/components/about/Education.js
+++ src/components/about/Education.js
@@ -20,12 +20,20 @@ export default function AboutEducation() {
                         <span className="text-gray-400 text-sm">Oct. 2020 - Oct. 2024</span>
                     </div>
 
-                    {/* Description */}
                     <p className="text-gray-300 mt-3">
-                        Worked on building scalable AI solutions for healthcare data,
-                        optimizing machine learning pipelines, and deploying production-ready
-                        models that improved diagnosis accuracy and efficiency.
+                        My MS research focused on developing a locomotion framework for quadrupedal robots by combining Reinforcement 
+                        Learning with Model Predictive Control, enabling adaptive and efficient movement in complex environments.
                     </p>
+
+                    <div className="flex flex-wrap pt-3 gap-2">
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Deep Learning</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Machine Learning</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Medical Robotics</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Medical AI</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Mobile Robotics</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">SLAM</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">ROS</span>
+                    </div>
                     </div>
 
 
@@ -40,12 +48,19 @@ export default function AboutEducation() {
                         <span className="text-gray-400 text-sm">Oct. 2015 - Aug. 2019</span>
                     </div>
 
-                    {/* Description */}
                     <p className="text-gray-300 mt-3">
-                        Worked on building scalable AI solutions for healthcare data,
-                        optimizing machine learning pipelines, and deploying production-ready
-                        models that improved diagnosis accuracy and efficiency.
+                        For my final year project, I developed a solution that would optimized locomotion gaits for quadrupedal robots using 
+                        a Genetic Algorithm. I used a neural network as the robot controller. 
                     </p>
+
+                    <div className="flex flex-wrap pt-3 gap-2">
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Deep Learning</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Machine Learning</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Electronic Devices</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Power Electronics</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Genetic Algorithm</span>
+                        <span className="px-3 py-1 rounded-full bg-gray-700 text-sm">Evolutionary Algorithms</span>
+                    </div>
                     </div>
 
             </section>
--- src/components/about/Head.js
+++ src/components/about/Head.js
@@ -4,9 +4,9 @@ export default function AboutHead() {
         <>
         <section className="mt-6 p-6 flex items-center gap-6">
             {/* Left side - Picture */}
-            <div className="w-32 h-32">
+            <div className="w-44 h-44">
                 <img
-                    // src="/profile/profile.JPG"
+                    src="/profile/profile.JPG"
                     alt="Image"
                     className="w-full h-full rounded-full object-cover border-4 border-gray-600 shadow-md"
                 />
@@ -14,8 +14,8 @@ export default function AboutHead() {
 
             {/* Right side - Name & Occupation */}
             <div>
-                <h1 className="text-4xl md:text-5xl font-bold mb-2">Name</h1>
-                <p className="text-xl text-gray-400">Thing 1 | Thing 2 | Thing 3</p>
+                <h1 className="text-4xl md:text-5xl font-bold mb-2">Zainullah Khan</h1>
+                <p className="text-xl text-gray-400">AI Developer | Creative Coder</p>
             </div>
         </section>
         </>
