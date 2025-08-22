aeaae7630635fc5b4f8de28cd7593d0a65570447
Zain <zain.9496@gmail.com>
2025-08-22 12:28:03 +0500

    Added summary section

--- src/components/about/ProfessionalSummary.js
+++ src/components/about/ProfessionalSummary.js
@@ -1,11 +1,11 @@
 export default function ProfessionalSummary() {
   return (
     <>
-        <section className="w-full bg-gray-800 border-gray-600 border-r-3 rounded-3xl bg-opacity-70 text-white shadow-md p-10">
-            <h1 className="text-4xl md:text-3xl font-bold mb-6">Summary</h1>
-            <p1>
-                I am an AI Developer based in Islamabad
-            </p1>
+        <section className="w-full bg-gray-800 border-gray-600 border-1 rounded-3xl bg-opacity-10 text-white shadow-md p-6">
+            <h2 className="text-3xl font-semibold mb-3">Summary</h2>
+            <p className="text-gray-400 max-w-2xl">
+                Summary about me
+            </p>
         </section>
     </>
   );
