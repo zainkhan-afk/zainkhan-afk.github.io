4522c5b5016634019205df326fa31218c699c841
Zain <zain.9496@gmail.com>
2025-08-22 16:34:45 +0500

    Cleanup

--- src/components/GlobalP5Wrapper.js
+++ src/components/GlobalP5Wrapper.js
@@ -2,7 +2,6 @@ export default function GlobalP5Wrapper() {
   return (
     <iframe
       src="https://zainkhan-afk.github.io/Simulations/Boids/index.html"
-    //   width="100%"
       className="absolute inset-0 w-full h-full z-0"
       style={{ border: "none" }}
     ></iframe>
