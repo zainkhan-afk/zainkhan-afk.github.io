04ca2a703337f72ca8a96b8dedf1b84d4b936144
Zain <zain.9496@gmail.com>
2025-08-25 13:08:15 +0500

    Updated gitignore. Also removed unnecessary sketches.

--- public/sketches/test_sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("p5-container"); // attach to the wrapper div
-}
-
-function draw() {
-  background(0);
-  ellipse(width / 2, height / 2, 50, 50);
-}
