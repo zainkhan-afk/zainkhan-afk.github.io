512e183222109f855c078e56c699635c52f6408f
Zain <zain.9496@gmail.com>
2025-08-25 12:39:21 +0500

    Added google robots.txt, sitemap.js and google console file.

---
+++ src/app/sitemap.js
@@ -0,0 +1,30 @@
+export default async function sitemap() {
+  const baseUrl = "https://zainkhan-afk.github.io";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/data-vis`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
