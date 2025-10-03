e2e1b369a1732605f9dba55a9e943c380c755de1
Zain <zain.9496@gmail.com>
2025-10-03 17:53:54 +0500

    Changed endpoint

--- src/api/chat.js
+++ src/api/chat.js
@@ -2,7 +2,7 @@
 
 export async function sendChatMessage(messages) {
   try {
-    const res = await fetch("http://127.0.0.1:8000/chat/", {
+    const res = await fetch("https://multi-project-api.vercel.app/chat/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({messages}),
