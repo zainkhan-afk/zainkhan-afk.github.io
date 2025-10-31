545d0c70d857025c1f17b18803f31c4fba8add10
Zain <zain.9496@gmail.com>
2025-10-31 13:55:18 +0500

    Changed sketch

--- src/components/GlobalP5Wrapper.js
+++ src/components/GlobalP5Wrapper.js
@@ -1,9 +1,11 @@
 export default function GlobalP5Wrapper() {
   return (
     <iframe
-      // src="https://zainkhan-afk.github.io/Simulations/Boids/index.html"
-      // src="https://zainkhan-afk.github.io/Simulations/Planets/index.html"
-      src = "/sketches/Planets/index.html"
+      
+      src = "/simulations/CollatzConjecture/index.html"
+      // src = "/simulations/Boids/index.html"
+      // src = "/simulations/Planets/index.html"
+      // src = "/simulations/Walking/index.html"
       className="absolute inset-0 w-full h-full z-0 border-0"
       style={{ overflow: "hidden" }}
       sandbox="allow-scripts allow-same-origin"
