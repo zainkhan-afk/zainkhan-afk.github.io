f60db861b6e51041e107a66cf8667ba07ffeb444
Zain <zain.9496@gmail.com>
2026-01-06 14:06:44 +0500

    Added initial code for day 6 of Genuary

---
+++ public/sketches/Genuary26/6-jan-light-off-on/mySketch.js
@@ -0,0 +1,16 @@
+
+function setup() 
+{
+	
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+    background(100);
+}
+
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
