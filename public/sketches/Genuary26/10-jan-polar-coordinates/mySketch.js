2d3d148cbe8c0c25b02556a65238130cb905a38f
Zain <zain.9496@gmail.com>
2026-01-11 01:08:43 +0500

    Basic implementation for jan 10 added.

--- public/sketches/Genuary26/10-jan-polar-coordinates/mySketch.js
+++ public/sketches/Genuary26/10-jan-polar-coordinates/mySketch.js
@@ -1,3 +1,6 @@
+let noiseMaxAmp = 1;
+let numPts = 50;
+let z = 0;
 function setup() 
 {
     let minDim = min(windowWidth, windowHeight);
@@ -8,6 +11,26 @@ function setup()
 function draw()
 {
     background(150, 150, 200);
+    translate(width/2, height/2);
+    beginShape();
+    for (let angle = 0; angle < TWO_PI; angle += TWO_PI / numPts){
+        let x = map(cos(angle), -1, 1, 0, noiseMaxAmp);
+        let y = map(sin(angle), -1, 1, 0, noiseMaxAmp);
+
+        let additionalMag = 0; 
+        if ((angle > PI/3 && angle < HALF_PI) || (angle > HALF_PI && angle < (HALF_PI + PI/3))){additionalMag = 100;}
+
+        console.log(additionalMag);
+
+        let mag1 = map(noise(x, y, z), 0, 1, 150 + additionalMag, 200 + additionalMag);
+        let mag2 = map(noise(x, y, z), 0, 1, 150, 200);
+
+        let ptX = mag1*cos(angle);
+        let ptY = mag1*sin(angle);
+        vertex(ptX, ptY);
+    }
+    endShape(CLOSE);
+    z += 0.01;
 }
 
 
