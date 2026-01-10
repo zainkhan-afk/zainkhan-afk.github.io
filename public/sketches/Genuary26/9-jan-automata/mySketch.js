c8b4e4a7a94c26259c967ca602e5e5c0614e66d3
Zain <zain.9496@gmail.com>
2026-01-10 22:31:06 +0500

    Completed Jan-9 prompt

--- public/sketches/Genuary26/9-jan-automata/mySketch.js
+++ public/sketches/Genuary26/9-jan-automata/mySketch.js
@@ -1,15 +1,16 @@
 let divH;
 let divW;
 let grid = [];
-let numRows = 50;
-let numCols = 50;
-let numStates = 5;
+let numRows = 200;
+let numCols = 200;
+let numStates = 3;
 let frameNum = 0;
+let recordingStarted = false;
 
 function setup() 
 {
     randomSeed(1);
-    let minDim = min(windowWidth, windowHeight)*2;
+    let minDim = min(windowWidth, windowHeight);
     createCanvas(minDim, minDim);
     divH = height / numRows;
     divW = width / numCols;
@@ -17,11 +18,16 @@ function setup()
     for (let r = 0; r < numRows; r++){
         let row = [];
         for (let c = 0; c < numCols; c++){
-            append(row, int(random(0, numStates-1)));
+            if (random() > 0.75){
+                append(row, int(random(0, numStates)));
+            }
+            else{
+                append(row, 0);
+            }
         }
         append(grid, row);
     }
-    frameRate(2);
+    frameRate(60);
 }
 
 function drawGrid(){
@@ -30,24 +36,40 @@ function drawGrid(){
         for (let c = 0; c < numCols; c++){
             let x = c*divW;
             let y = r*divH;
-            // console.log("drawing", c, r);
-            // fill(0, 0, grid[r][c] / numStates * 200)
-            fill(255, grid[r][c] / numStates * 200, 0);
+            
+            
+            
+            
             push();
             translate(x, y);
-
-            let vertices = VertexMaker.GetVertices(grid[r][c], divH/2);
             
-            beginShape();
-            for (let v of vertices){
-                vertex(v[0], v[1]);
+
+
+
+
+
+
+
+
+
+
+            
+            fill(255, grid[r][c] / numStates * 200, 0);
+            fill(0, 0, 0);
+            if (grid[r][c] == 1){
+                fill(0, 0, 200);
             }
-            endShape();
-            // fill(200);
+            else if (grid[r][c] == 2){
+                fill(250, 100, 150);
+            }
+            // let vertices = VertexMaker.GetVertices(grid[r][c], divH/2);
+            
+            // beginShape();
+            // for (let v of vertices){
+            //     vertex(v[0], v[1]);
+            // }
+            // endShape();
             rect(0, 0, divH, divW);
-            // fill(0);
-            // textSize(25);
-            // text(grid[r][c], divW/2, divH/2);
             pop();
         }
     }
@@ -78,7 +100,7 @@ function updateGrid(){
         let row = [];
         for (let c = 0; c < numCols; c++){
             let newVal;
-            if ((r + c) % (1 + frameNum%2) == 0){
+            if ((r + c) % (1+frameNum%2) == 0){
                 let highestDiffNeighbor = getHighestDiffNeighbor(r, c);
                 if (highestDiffNeighbor.length > 0){
                     let diff = grid[r][c] - grid[highestDiffNeighbor[0]][highestDiffNeighbor[1]]
@@ -103,34 +125,89 @@ function updateGrid(){
     grid = newGrid;
 }
 
+function neightborhoodCheck(R, C){
+    let numNeighbors = 0;
+    let populationCounter = 0;
+    let numMales = 0;
+    let numFemales = 0;
+    for (let r = max(R - 1, 0); r < min(R + 2, numRows); r++){
+        for (let c = max(C - 1, 0); c < min(C + 2, numCols); c++){
+            if (c == C && r == R){continue;}
+            numNeighbors += 1;
+            if (grid[r][c] != 0){ populationCounter += 1; }
+            if (grid[r][c] == 1) { numMales += 1; }
+            if (grid[r][c] == 2) { numFemales += 1; }
+        }
+    }
+    if (populationCounter > (numNeighbors*0.7)  && grid[R][C] != 0){return {kill : true, spawn : false};}
+    if (numFemales > 0 && numMales > 0 && grid[R][C] == 0){return {kill : false, spawn : true};}
+    // if (numMales > 0 && grid[R][C] == 1){
+    //     if (random() < 0.1){
+    //         return {kill : true, spawn : false};
+    //     }
+    //     else{
+    //         return {kill : false, spawn : false};
+    //     }
+    // }
+    // if (numFemales > 0 && grid[R][C] == 2){
+    //     if (random() < 0.1){
+    //         return {kill : true, spawn : false};
+    //     }
+    //     else{
+    //         return {kill : false, spawn : false};
+    //     }
+    // }
+    return {kill : false, spawn : false};
+
+    // return sum;
+}
+
+function updateGrid2(){
+    let newGrid = [];
+    for (let r = 0; r < numRows; r++){
+        let row = [];
+        for (let c = 0; c < numCols; c++){
+            let newVal;
+
+            let res = neightborhoodCheck(r, c);
+            if (res.spawn){
+                if (random() < 0.2){
+                    newVal = random() > 0.5 ? 1 : 2;
+                }
+                else{
+                    newVal = 0;
+                }
+            }
+            else if (res.kill){
+                newVal = 0;
+            }
+            else{
+                newVal = grid[r][c];
+            }
+
+            append(row, newVal);
+        }
+        append(newGrid, row);
+    }   
+    grid = newGrid;
+}
+
 function draw()
 {
+    if (!recordingStarted){
+        recordingStarted = true;
+        saveGif('automata', 30, { delay: 0 });
+    }
     background(150, 150, 200);
     drawGrid();
-    updateGrid();
+    updateGrid2();
 
-    // push();
-    // scale(0.5, 0.5);
-    // drawGrid();
-    // pop();
-    // updateGrid();
-    // push();
-    // translate(width/2, 0);
-    // scale(0.5, 0.5);
-    // stroke(255, 0, 0);
-    // strokeWeight(10);
-    // line(0, 0, 0, height);
-    // stroke(0);
-    // strokeWeight(1);
-    // drawGrid();
-    // pop();
-    // noLoop();
     frameNum += 1;
 }
 
 
 function keyPressed() {
   if (key === 's') {
-    // saveGif('mySketch', 20, { delay: 0 });
+    saveGif('mySketch', 5, { delay: 0 });
   }
 }
--- public/sketches/Genuary26/9-jan-automata/vertexMaker.js
+++ public/sketches/Genuary26/9-jan-automata/vertexMaker.js
@@ -1,13 +1,17 @@
 class VertexMaker{
     static GetVertices(n, r){
-        let angleDiff = TWO_PI / n;
         let vertices = [];
 
-        for (let angle = 0; angle < TWO_PI; angle += angleDiff){
-            let x = r*cos(angle);
-            let y = r*sin(angle);
+        if (n == 0){return append(vertices, [0, 0]);}
+        else{
+            let angleDiff = TWO_PI / n;
 
-            append(vertices, [x, y]);
+            for (let angle = 0; angle < TWO_PI; angle += angleDiff){
+                let x = r*cos(angle);
+                let y = r*sin(angle);
+
+                append(vertices, [x, y]);
+            }
         }
 
         return vertices;
