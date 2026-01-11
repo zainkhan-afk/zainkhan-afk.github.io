ec3984321ea24bb231c4061b9a5c3ba5dc5a0caf
Zain <zain.9496@gmail.com>
2026-01-11 21:14:22 +0500

    Completed jan-11 prompt

--- public/sketches/Genuary26/11-jan-quine/mySketch.js
+++ public/sketches/Genuary26/11-jan-quine/mySketch.js
@@ -1,13 +1,18 @@
 let title = "A Quine In P5js";
 
+let lines, cnv;
 let dim = 20;
 let numRows;
 let numCols;
 
+function preload() {
+  lines = loadStrings('mySketch.js');
+}
+
 function setup() 
 {
     let minDim = min(windowWidth, windowHeight);
-    createCanvas(minDim, minDim);
+    cnv = createCanvas(minDim, minDim*1.5);
 
     numRows = int(height / dim) + 1;
     numCols = int(width / dim) + 1;
@@ -18,6 +23,7 @@ function setup()
 function draw()
 {
   background(220);
+  textAlign(LEFT, TOP);
 
   noStroke();
   fill(100, 100);
@@ -30,11 +36,27 @@ function draw()
     }
   }
 
+  fill(0);
+  stroke(0);
+  
+  let offsetY = 10;
+  let offsetX = 50;
+  // textSize(25);
+  for (let i = 0; i < lines.length; i++) {
+    if (i == 0){textSize(30);}
+    else{textSize(15);}
+
+    text(lines[i], offsetX, i*20 + offsetY);
+    if (i == 0) {offsetY += 20;}
+    if (i == 6 || i == 21) {offsetY += 0; offsetX += 300; }
+    if (i == 10 || i == 55) {offsetY += 0; offsetX -= 300; }
+  }
 }
 
 
 function keyPressed() {
   if (key === 's') {
-    saveGif('mySketch', 10, { delay: 0 });
+    saveCanvas(cnv, '11-jan-quine.jpg');
+
   }
 }
