0a9bdabdcfcc7ee9625d7fd72c0153b8ac836ffb
Zain <zain.9496@gmail.com>
2026-01-17 01:00:27 +0500

    Completed Gen15

--- public/sketches/Genuary26/15-jan-invisible-object/boid.js
+++ public/sketches/Genuary26/15-jan-invisible-object/boid.js
@@ -1,10 +1,18 @@
 class Boid{
     constructor(pos){
         this.pos = pos;
-        this.vel = createVector(0, 1, 0);
+        // this.vel = createVector(random(-30, 30), random(-30, 30), random(-30, 30));
+        this.maxVel = 100;
+        this.minVel = this.maxVel * 0.7;
+        this.vel = p5.Vector.fromAngle(random()*TWO_PI, random(0.7*this.maxVel, this.maxVel))
     }
 
     Update(dt){
+        this.vel.limit(this.maxVel);
+        if (this.vel.mag() < this.minVel){
+            this.vel.setMag(this.minVel);
+        }
+        // console.log(this.vel.mag());
         this.pos.add(p5.Vector.mult(this.vel, dt));
     }
 }
--- public/sketches/Genuary26/15-jan-invisible-object/mySketch.js
+++ public/sketches/Genuary26/15-jan-invisible-object/mySketch.js
@@ -1,9 +1,17 @@
 let cam;
 let boids = [];
-let numBoids = 10;
-let avoidFactor = 0.1;
-let matchingFactor = 2;
+let numBoids = 300;
+
 let visibleDistance = 50;
+let protectedDistance = visibleDistance*0.8;
+
+let avoidFactor = 1;
+let matchingFactor = 0.3;
+let centeringfactor = 0.1;
+
+let invisibleObjectDims = 500;
+
+let offset = 0;
 
 function preload(){
 }
@@ -16,7 +24,7 @@ function setup()
 
   
   for (let i = 0; i < numBoids; i++){
-    append(boids, new Boid(createVector(random(-10, 10), random(-10, 10), random(80, 120))));
+    append(boids, new Boid(createVector(random(-300, 300), random(-300, 300), random(80, 120))));
   }
   
   frameRate(60);
@@ -24,6 +32,10 @@ function setup()
 
 function draw()
 {
+  // let val = sin(offset);
+  // matchingFactor = map(val, -1, 1, 0.1, 1.3);
+  // avoidFactor = map(-val, -1, 1, 1, 2);
+  // offset += 0.01;
   perspective(0.9, 1.0, 1, 12000);
 
   lights();
@@ -33,6 +45,13 @@ function draw()
   directionalLight(200, 200, 100, -0.5, -0.5, -0.5);
   orbitControl();
   
+  push();
+  translate(0, 0, invisibleObjectDims/2);
+  noFill();
+  stroke(0);
+  strokeWeight(3);
+  box(invisibleObjectDims);
+  pop();
   
   
   noStroke();
@@ -40,13 +59,23 @@ function draw()
   plane(1000);
 
   for (let boid of boids){
-    fill(100, 0, 0);
+    if (boid.pos.x > invisibleObjectDims/2 ||
+       boid.pos.x < -invisibleObjectDims/2 || 
+       boid.pos.y > invisibleObjectDims/2 || 
+       boid.pos.y < -invisibleObjectDims/2){
+      fill(100, 0, 0);
+      noStroke();
+    }
+    else{
+      stroke(0);
+      noFill();
+    }
     push();
     translate(boid.pos.x, boid.pos.y, boid.pos.z);
     box(10);
     pop();
+    
     fill(0);
-
     push();
     translate(boid.pos.x, boid.pos.y, 1);
     rect(0, 0, 10, 10);
@@ -61,35 +90,75 @@ function draw()
 }
 
 function BoidUpdate(){
+  let turnFactor = 10;
   let index1 = 0;
   for (let boid1 of boids){
     let close = createVector(0, 0, 0);
     let velAvg = createVector(0, 0, 0);
+    let posAvg = createVector(0, 0, 0);
+    let turnVel = createVector(0, 0, 0);
 
     let neighbors = 0;
 
     let index2 = 0;
     for (let boid2 of boids){
       if (index1 === index2){index2 += 1; continue;}
-      close.add(p5.Vector.sub(boid1.pos, boid2.pos));
-
-      if (p5.Vector.dist(boid1.pos, boid2.pos) < visibleDistance){
-        velAvg.add(boid2.vel);
-        neighbors += 1;
+      let boidDist = p5.Vector.dist(boid1.pos, boid2.pos);
+      if ( boidDist < visibleDistance){
+        if (boidDist < protectedDistance){
+          close.add(p5.Vector.sub(boid1.pos, boid2.pos));
+        }
+        else{
+          velAvg.add(boid2.vel);
+          posAvg.add(boid2.pos);
+          neighbors += 1;
+        }
       }
 
       index2 += 1;
     }
-    close.mult(avoidFactor);
     
     if (neighbors > 0){
       velAvg.mult(1/neighbors);
+      posAvg.mult(1/neighbors);
+      
       velAvg.sub(boid1.vel);
-      velAvg.mult(matchingFactor)
+      posAvg.sub(boid1.pos);
+      
+    }
+    velAvg.mult(matchingFactor)
+    posAvg.mult(centeringfactor)
+    close.mult(avoidFactor);
+    
+    if (boid1.pos.x < -400){
+      turnVel.x = turnFactor; 
+    }
+    else if (boid1.pos.x > 400){
+      turnVel.x = -turnFactor; 
+    }
+
+    if (boid1.pos.y < -400){
+      turnVel.y = turnFactor; 
+    }
+    else if (boid1.pos.y > 400){
+      turnVel.y = -turnFactor; 
+    }
+
+
+    if (boid1.pos.z < 50){
+      turnVel.z = turnFactor; 
+    }
+    else if (boid1.pos.z > 300){
+      turnVel.z = -turnFactor; 
     }
     
     boid1.vel.add(velAvg);
     boid1.vel.add(close);
+    boid1.vel.add(posAvg);
+    
+    // boid1.vel.limit(boid1.maxVel);
+
+    boid1.vel.add(turnVel);
     index1 += 1;
   }
 }
@@ -97,10 +166,18 @@ function BoidUpdate(){
 
 function AdjustCamera(){
   cam = createCamera();
-  cam.camera(500, 500, 500, 
+  cam.camera(800, 800, 600, 
              0, 0, 0, 
              0, 0, -1);
 
   cam.ortho();
   setCamera(cam);
+}
+
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif("Gen15", 10);
+
+  }
 }
