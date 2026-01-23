6cbbe697865ad08ff86feeb5fc738ab2df2e2210
Zain <zain.9496@gmail.com>
2026-01-23 23:52:33 +0500

    Added basic code for Jan20

---
+++ public/sketches/Genuary26/20-jan/mySketch.js
@@ -0,0 +1,24 @@
+let cnv;
+
+function preload(){
+}
+
+function setup() 
+{
+  let minDim = min(windowWidth, windowHeight);
+  cnv = createCanvas(minDim, minDim);
+  
+  frameRate(60);
+}
+
+function draw()
+{
+  background(200);
+}
+
+function keyPressed() {
+  if (key === 's') {
+    // saveCanvas(cnv, '17-jan.jpg');
+    saveGif("Gen19", 10);
+  }
+}
