5ba11c10dced1c9000bea6832f68af3f1b4c005f
Zain <zain.9496@gmail.com>
2026-01-24 20:13:08 +0500

    Added basic background, shapes and colors

--- public/sketches/Genuary26/21-jan-bauhaus/mySketch.js
+++ public/sketches/Genuary26/21-jan-bauhaus/mySketch.js
@@ -1,24 +1,50 @@
 let cnv;
+let red;
+let blue;
+let yellow;
+let black;
+
+let div = 50;
+let numRows;
+let numCols;
 
 function setup() 
 {
     let minDim = min(windowWidth, windowHeight);
     cnv = createCanvas(minDim, minDim);
+
+    numRows = int(height / div) + 1;
+    numCols = int(width / div) + 1;
+
+    red = color(250, 0, 0);
+    blue = color(0, 0, 255);
+    yellow = color(255, 204, 0);
+    black = color(0, 0, 0);
+
     frameRate(60);
-}
-
-function resetParamsAt(i){
-    endPosX[i] = 0;
-    endPosY[i] = 0;
-    startPosX[i] = width / 2 + random(- width * 0.01, width * 0.01);
-    startPosY[i] = height - 100;
-
-    ang[i] = -PI / 2;
-    frameNum[i] = 0;
-    // angleOffset[i] *= -1;
 }
 
 function draw()
 {
     background(240, 240, 200);
+    stroke(0);
+    for(let c = 0; c < numCols; c++){
+        line(c*div, 0, c*div, height);
+    }
+    
+    noStroke();
+
+    fill(red);
+    circle(random(200, width - 200), random(200, height - 200), 200);
+
+    fill(blue);
+    circle(random(200, width - 200), random(200, height - 200), 200);
+
+    fill(yellow);
+    circle(random(200, width - 200), random(200, height - 200), 200);
+
+    fill(black);
+    circle(random(200, width - 200), random(200, height - 200), 200);
+
+    noLoop();
 }
