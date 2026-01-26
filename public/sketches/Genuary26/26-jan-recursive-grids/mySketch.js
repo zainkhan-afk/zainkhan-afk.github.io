24044ade7995a1166efa37bdd241e2f6e22b58b6
Zain <zain.9496@gmail.com>
2026-01-27 00:16:38 +0500

    Completed Gen26 with bauhaus colors

--- public/sketches/Genuary26/26-jan-recursive-grids/mySketch.js
+++ public/sketches/Genuary26/26-jan-recursive-grids/mySketch.js
@@ -1,16 +1,50 @@
+
 let cnv;
+let div = 100;
+let numRows;
+let numCols;
+
+let frameNum = 0;
+let gridCtr = 0;
+
+let colors = [];
 
 function setup() 
 {
-    let minDim = min(windowWidth, windowHeight);
-    cnv = createCanvas(minDim, minDim);    
+  let minDim = min(windowWidth, windowHeight);
+  cnv = createCanvas(minDim, minDim);
+
+  colors = [color(200, 0, 0), color(0, 0, 200), color(200, 170, 0), color(0, 0, 0)];
+
+  numRows = int(height / div) + 1;
+  numCols = int(width / div) + 1;
+
+  frameRate(60);
 }
 
 
 
 function draw()
 {
-  background(0);
+  // background(0);
+  for (let i = 0; i < 10; i++){
+    if (frameNum < numRows*numCols){
+      let colorIdx = int(random(colors.length));
+      let r = int(frameNum / numCols);
+      let c = frameNum % numCols;
+
+      fill(colors[gridCtr%colors.length]);
+      rect(c*div, r*div, div, div);
+    }
+    else{
+      div = int(div / 2);
+      numRows = int(height / div) + 1;
+      numCols = int(width / div) + 1;
+      frameNum = 0;
+    }
+
+    frameNum += 1;
+  }
 }
 
 
