4612d45c8cf7bb6d34d2accef6214b26f2459216
Zain <zain.9496@gmail.com>
2026-03-16 13:56:36 +0500

    Minor changes to bauhaus poster

--- public/sketches/Genuary26/21-jan-bauhaus/bauhausPoster.js
+++ public/sketches/Genuary26/21-jan-bauhaus/bauhausPoster.js
@@ -4,12 +4,17 @@ class BauhausPoster{
         this.blue = blue;
         this.yellow = yellow;
         this.black = black;
+        this.polygonSides = 8;
+        this.numRings = 3;
+        this.ringConstantSize = 100;
+        this.ringSizeDiff = 40;
         this.orange = color(240, 120, 0);
+        this.ringYOffset = 0;
     }
 
     RenderConcentricCirlces(){
         noStroke();
-        for(let i = 5; i > 0; i--){
+        for(let i = this.numRings; i > 0; i--){
             let r = this.orange.levels[0];
             let g = this.orange.levels[1];
             let b = this.orange.levels[2];
@@ -20,22 +25,54 @@ class BauhausPoster{
     }
 
     RenderConcentricHexagons(){
-        for(let i = 5; i > 0; i--){
+        noStroke();
+        for(let i = this.numRings; i > 0; i--){
             push();
-            translate(width/2, height/2 - i*20);
+            translate(width/2, height/2 - i*this.ringYOffset);
+            rotate(PI/this.polygonSides);
+
             let r = this.orange.levels[0];
             let g = this.orange.levels[1];
             let b = this.orange.levels[2];
             let a = this.orange.levels[3];
-            fill(r, g, b, a - i*30);
-            stroke(0, i*30);
+            fill(r, g, b, a-(i+1)*40);
+            // stroke(0, i*30);
+            // circle(0, 0, i*2*50);
             beginShape();
-            for (let j = 0; j < 6; j++){
-                let x = 50*i*cos(j*TWO_PI/6);
-                let y = 50*i*sin(j*TWO_PI/6);
+            for (let j = 0; j < this.polygonSides; j++){
+                let x = (this.ringConstantSize + this.ringSizeDiff*i)*cos(j*TWO_PI/this.polygonSides);
+                let y = (this.ringConstantSize + this.ringSizeDiff*i)*sin(j*TWO_PI/this.polygonSides);
                 vertex(x, y);
             }
+            
             endShape(CLOSE);
+            pop();
+        }
+
+        noFill();
+        for(let i = this.numRings; i > 1; i--){
+            let a = this.orange.levels[3];
+            stroke(0, i*30);
+            push();
+            translate(width/2, height/2 - (i)*this.ringYOffset);
+            rotate(PI/this.polygonSides);
+
+            for (let j = 0; j < this.polygonSides; j++){
+                let x1 = (this.ringConstantSize + this.ringSizeDiff*i)*cos(j*TWO_PI/this.polygonSides);
+                let y1 = (this.ringConstantSize + this.ringSizeDiff*i)*sin(j*TWO_PI/this.polygonSides);
+                let x2 = (this.ringConstantSize + this.ringSizeDiff*(i-1))*cos(j*TWO_PI/this.polygonSides);
+                let y2 = (this.ringConstantSize + this.ringSizeDiff*(i-1))*sin(j*TWO_PI/this.polygonSides) + this.ringYOffset;
+                // line(x1, y1, x2, y2);
+                // beginShape();
+                // for(let i = 5; i > 0; i--){
+                //     // push();
+                //     let x = width/2 + 50*i*cos(j*TWO_PI/6);
+                //     let y = height/2 - i*yOff + 50*i*sin(j*TWO_PI/6);
+                //     vertex(x, y);
+                //     // pop();
+                // }
+                // endShape();
+            }
             pop();
         }
 
@@ -53,31 +90,31 @@ class BauhausPoster{
     }
 
     RenderLines(){
-        push();
-        translate(width/2, height/2);
-        rotate(TWO_PI/18);
         stroke(0);
         fill(0);
-        for (let i = 5; i>0; i--){
-            
-            for (let j = 0; j<6; j++)
+        for (let i = this.numRings; i>0; i--){
+            push();
+            translate(width/2, height/2 - i*this.ringYOffset);
+            rotate(TWO_PI/(this.polygonSides*1.5));
+
+            for (let j = 0; j<this.polygonSides; j++)
             {
-                let angle = j*TWO_PI/6;
+                let angle = j*TWO_PI/this.polygonSides;
                 beginShape();
                 for (let k = 0; k<4; k++){
                     let offset = 0;
                     let p = 0;
                     if (k == 2 || k==3){offset = TWO_PI/18;}
-                    if (k == 1 || k == 2) { p -= 5;}
+                    if (k == 1 || k == 2) { p -= 20;}
 
-                    let x = i*((50-6)+p)*cos(angle + offset);
-                    let y = i*((50-6)+p)*sin(angle + offset); 
+                    let x = i*((this.ringConstantSize-6)+p)*cos(angle + offset);
+                    let y = i*((this.ringConstantSize-6)+p)*sin(angle + offset); 
                     vertex(x, y);
                 }
                 endShape();
             }
+            pop();
         }
-        pop();
     }
 
     RenderMan(){
