7dcf4ace3f7b404debda69a0f0ea4afe56ffdd2e
Zain <zain.9496@gmail.com>
2026-03-16 14:52:38 +0500

    Changed Bauhaus poster

--- public/sketches/Genuary26/21-jan-bauhaus/bauhausPoster.js
+++ public/sketches/Genuary26/21-jan-bauhaus/bauhausPoster.js
@@ -5,27 +5,56 @@ class BauhausPoster{
         this.yellow = yellow;
         this.black = black;
         this.polygonSides = 8;
-        this.numRings = 3;
+        this.numRings = 4;
         this.ringConstantSize = 100;
         this.ringSizeDiff = 40;
         this.orange = color(240, 120, 0);
         this.ringYOffset = 0;
     }
 
-    RenderConcentricCirlces(){
-        noStroke();
-        for(let i = this.numRings; i > 0; i--){
-            let r = this.orange.levels[0];
-            let g = this.orange.levels[1];
-            let b = this.orange.levels[2];
-            let a = this.orange.levels[3];
-            fill(r, g, b, a - i*30);
-            circle(width/2, height/2, 100*i);
+    RenderSquiglyLines(yStart){
+        push();
+        // rotate(PI/60);
+        noFill();
+        strokeWeight(2);
+        // for (let y = -100; y < height+100; y+=5){
+        // console.log(height, int(height/2), height%5);
+        // for (let y = int((height-height%10)/2); y < height+100; y+=5){
+        for (let y = yStart; y < height+100; y+=5){
+            if (y%10 == 0){
+                stroke(this.blue);
+            }
+            else if (y%5 == 0){
+                stroke(this.orange);
+            }
+            beginShape();
+            for (let x = 0; x < width*1.5; x+=1){
+                let n = map(noise(x/width*30, y/height*5), 0, 1, -1, 1)*20;
+                // let n = noise(x+y);
+                vertex(x, y+n);
+            }
+            endShape();
         }
+        strokeWeight(1);
+        pop();
     }
 
     RenderConcentricHexagons(){
         noStroke();
+        push();
+        translate(width/2, height/2);
+        rotate(PI/this.polygonSides);
+
+        fill(200, 210, 200);
+        beginShape();
+        for (let j = 0; j < this.polygonSides; j++){
+            let x = (this.ringConstantSize + this.ringSizeDiff*(this.numRings))*cos(j*TWO_PI/this.polygonSides);
+            let y = (this.ringConstantSize + this.ringSizeDiff*(this.numRings))*sin(j*TWO_PI/this.polygonSides);
+            vertex(x, y);
+        }
+        endShape();
+        pop();
+
         for(let i = this.numRings; i > 0; i--){
             push();
             translate(width/2, height/2 - i*this.ringYOffset);
@@ -36,7 +65,7 @@ class BauhausPoster{
             let b = this.orange.levels[2];
             let a = this.orange.levels[3];
             fill(r, g, b, a-(i+1)*40);
-            // stroke(0, i*30);
+            stroke(0, i*30);
             // circle(0, 0, i*2*50);
             beginShape();
             for (let j = 0; j < this.polygonSides; j++){
@@ -62,82 +91,46 @@ class BauhausPoster{
                 let y1 = (this.ringConstantSize + this.ringSizeDiff*i)*sin(j*TWO_PI/this.polygonSides);
                 let x2 = (this.ringConstantSize + this.ringSizeDiff*(i-1))*cos(j*TWO_PI/this.polygonSides);
                 let y2 = (this.ringConstantSize + this.ringSizeDiff*(i-1))*sin(j*TWO_PI/this.polygonSides) + this.ringYOffset;
-                // line(x1, y1, x2, y2);
-                // beginShape();
-                // for(let i = 5; i > 0; i--){
-                //     // push();
-                //     let x = width/2 + 50*i*cos(j*TWO_PI/6);
-                //     let y = height/2 - i*yOff + 50*i*sin(j*TWO_PI/6);
-                //     vertex(x, y);
-                //     // pop();
-                // }
-                // endShape();
+                line(x1, y1, x2, y2);
             }
             pop();
         }
 
-
-
-        // fill(180);
-        // beginShape();
-        // for (let j = 0; j < 6; j++){
-        //     let x = 25*cos(j*TWO_PI/6);
-        //     let y = 25*sin(j*TWO_PI/6);
-        //     vertex(x, y);
-        // }
-        // endShape(CLOSE);
-
     }
 
-    RenderLines(){
+    RenderFont(){
         stroke(0);
-        fill(0);
-        for (let i = this.numRings; i>0; i--){
-            push();
-            translate(width/2, height/2 - i*this.ringYOffset);
-            rotate(TWO_PI/(this.polygonSides*1.5));
+        fill(this.black);
+        textSize(40);
+        textAlign(CENTER, CENTER);
+        textStyle(BOLD);
+        // textFont(this.urduFont)
+        for ( let i = 0; i < genuary.length; i ++){
+            // text(genuary[i], 120, 50 + 40*i);
+            text(this.urduText, 80, 50 + 40*i);
+            // break;
 
-            for (let j = 0; j<this.polygonSides; j++)
-            {
-                let angle = j*TWO_PI/this.polygonSides;
-                beginShape();
-                for (let k = 0; k<4; k++){
-                    let offset = 0;
-                    let p = 0;
-                    if (k == 2 || k==3){offset = TWO_PI/18;}
-                    if (k == 1 || k == 2) { p -= 20;}
-
-                    let x = i*((this.ringConstantSize-6)+p)*cos(angle + offset);
-                    let y = i*((this.ringConstantSize-6)+p)*sin(angle + offset); 
-                    vertex(x, y);
-                }
-                endShape();
-            }
-            pop();
         }
-    }
 
-    RenderMan(){
-        let headDiameter = 30;
-        push();
-        translate(width/2, height/2);
-        fill(200);
-        ellipse(0, 0, headDiameter/1.2, headDiameter);
+        textFont(font);
+        let h = 100;
+        noFill();
+        noStroke();
+        fill(240, 240, 200)
+        rect(0, height - h, width, h);
 
-        fill(0);
-        beginShape();
-        vertex(-headDiameter/3, -headDiameter/5);
-        vertex(headDiameter/3, -headDiameter/5);
-        vertex(headDiameter/3, headDiameter/5);
-        vertex(-headDiameter/3, headDiameter/5);
-        endShape();
-        pop();
+        stroke(0);
+        fill(this.black);
+        textSize(30);
+        text("Genuary", 100, height - h/2);
+        text("2026", width - 100, height - h/2);
     }
 
     Render(){
-        // this.RenderConcentricCirlces();
+        this.RenderSquiglyLines(-100);
         this.RenderConcentricHexagons();
-        // this.RenderLines();
-        this.RenderMan();
+        this.RenderSquiglyLines(int((height-height%10)/2));
+        this.RenderFont();
+
     }
 }
